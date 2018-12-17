ada78e7d15dc79949f6912dc03b3a72b51afac46
Denis <d.e.min@yandex.ru>
2018-12-17 13:23:11 +0300

    подключил script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-    var message_link = document.querySelector(".link-write-us");
-    var popup_message
-    if (popup_message = document.querySelector(".modal-message")) {
-    var close_popup_message = popup_message.querySelector(".modal-close");}
-
-    message_link.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup_message.classList.add("modal-show");
-      popup_message.classList.add("modal-animation");
-    })
-
-    close_popup_message.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup_message.classList.remove("modal-show");
-      popup_message.classList.remove("modal-animation");
-      popup_message.classList.remove("form-error");
-    })
-
-    var form_message = popup_message.querySelector("form");
-    var user_name = form_message.querySelector("[name=user-name]");
-    var user_mail = form_message.querySelector("[name=user-mail]");
-    var user_message = form_message.querySelector("[name=user-message]");
+    if (document.querySelector(".modal-message") !== null) {
+      var popup_message = document.querySelector(".modal-message");
+      var close_popup_message = popup_message.querySelector(".modal-close");
+      var form_message = popup_message.querySelector("form");
+      var user_name = form_message.querySelector("[name=user-name]");
+      var user_mail = form_message.querySelector("[name=user-mail]");
+      var user_message = form_message.querySelector("[name=user-message]");
 
     form_message.addEventListener("submit", function(evt) {
       if (!user_name.value || !user_mail.value || !user_message.value) {
@@ -29,36 +14,53 @@
         popup_message.classList.add("form-error");
       }
     })
+  }
 
-    var map_link = document.querySelector(".map-link");
-    var popup_map = document.querySelector(".modal-map");
-    var close_popup_map = popup_map.querySelector(".modal-close");
-
-    map_link.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup_map.classList.add("modal-show");
+    if (document.querySelector(".link-write-us") !== null) {
+      var message_link = document.querySelector(".link-write-us");
+      message_link.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popup_message.classList.add("modal-show");
+        popup_message.classList.add("modal-animation");
     })
-
-    close_popup_map.addEventListener("click", function(evt) {
+    close_popup_message.addEventListener("click", function(evt) {
       evt.preventDefault();
-      popup_map.classList.remove("modal-show");
+      popup_message.classList.remove("modal-show");
+      popup_message.classList.remove("modal-animation");
+      popup_message.classList.remove("form-error");
     })
+  }
 
-    var buy_items = document.querySelectorAll(".button-buy");
+  if (document.querySelector(".modal-map") !== null) {
+      var popup_map = document.querySelector(".modal-map");
+      var map_link = document.querySelector(".map-link");
+      var close_popup_map = popup_map.querySelector(".modal-close");
+      map_link.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popup_map.classList.add("modal-show");
+    })
+      close_popup_map.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popup_map.classList.remove("modal-show");
+    })
+  }
+
+  if (document.querySelector(".modal-kart") !== null) {
     var popup_kart = document.querySelector(".modal-kart");
+    var buy_items = document.querySelectorAll(".button-buy");
     var close_popup_kart = popup_kart.querySelector(".modal-close");
-
     for (var i = 0; i < buy_items.length; i++) {
       var buy_item = buy_items[i];
       buy_item.addEventListener("click", click_on_button);
     }
 
-     function click_on_button(evt) {
-      evt.preventDefault();
-      popup_kart.classList.add("modal-show");
+      function click_on_button(evt) {
+        evt.preventDefault();
+        popup_kart.classList.add("modal-show");
     }
 
-    close_popup_kart.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      popup_kart.classList.remove("modal-show");
+      close_popup_kart.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        popup_kart.classList.remove("modal-show");
     })
+      }
